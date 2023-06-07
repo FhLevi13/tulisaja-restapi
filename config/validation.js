28a93b9aec35d3f18e00343143e20beaf1f5db06
FhLevi13 <alifvahlephi46@gmail.com>
2023-06-07 10:23:48 +0700

    up

--- config/validation.js
+++ config/validation.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 const registerValidation = (data) => {
     const schema = Joi.object({
         username: Joi.string().required(),
-        password: Joi.string().min().required()
+        password: Joi.string().min(6).required()
     })
 
     return schema.validate(data)
@@ -12,7 +12,7 @@ const registerValidation = (data) => {
 const loginValidation = (data) => {
     const schema = Joi.object({
         username: Joi.string().required(),
-        password: Joi.string().min().required()
+        password: Joi.string().min(6).required()
     })
 
     return schema.validate(data)
